96fe8384e8cd5bad362b724d90fb5acead9e3130
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-08-13 11:55:57 +0900

    refactor: 상수 이름 변경

--- src/main/java/baseball/view/ErrorMessage.java
+++ src/main/java/baseball/view/ErrorMessage.java
@@ -2,10 +2,10 @@ package baseball.view;
 
 public enum ErrorMessage {
 
-    NUMBER_RANGE("숫자는 1 ~ 9 사이의 범위여야 합니다."),
-    NUMBER_DUPLICATE("숫자는 중복될 수 없습니다."),
-    NUMBER_SIZE("숫자는 3자리 수여야 합니다."),
-    NUMBER_TYPE("입력값은 숫자로만 구성되어야 합니다.");
+    ERR_NUMBER_RANGE("숫자는 1 ~ 9 사이의 범위여야 합니다."),
+    ERR_NUMBER_DUPLICATE("숫자는 중복될 수 없습니다."),
+    ERR_NUMBER_SIZE("숫자는 3자리 수여야 합니다."),
+    ERR_NUMBER_TYPE("입력값은 숫자로만 구성되어야 합니다.");
 
     private final String message;
 
