2ec7ffbeb71aaec0536a78dd9f9c6e11f4853386
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-08-17 17:04:31 +0900

    refactor: 개행 문자 추가

--- src/main/java/baseball/view/messages/GameMessage.java
+++ src/main/java/baseball/view/messages/GameMessage.java
@@ -2,7 +2,7 @@ package baseball.view.messages;
 
 public enum GameMessage {
 
-    GAME_START("숫자 야구 게임을 시작합니다."),
+    GAME_START("숫자 야구 게임을 시작합니다.\n"),
     GAME_INPUT("숫자를 입력해주세요 : "),
     GAME_RESTART("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
 
